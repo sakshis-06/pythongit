f8f2e8a2e07fd12437b3792e23881b0ebae18fc1
sakshis-06 <115691471+sakshis-06@users.noreply.github.com>
2022-10-13 13:11:14 +0530

    Add files via upload

---
+++ p8.py
@@ -0,0 +1,30 @@
+#functions
+def myFunc():
+    print("this is my function")
+myFunc()
+
+def add():
+    a=4
+    b=6
+    return  a*b
+r=add()
+print(r)
+
+
+def add(a,b):
+    r= a+b
+    print(r)
+add(3,4)
+
+
+#math fucntions
+from math import  *
+n=25
+print(sqrt(n))
+print(factorial(5))
+print(pow(2,4))
+#sin,tan,cos,fabs ,pow,factorial,sqrt etc
+
+
+
+
