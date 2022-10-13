99c25b2c82ee40e3ca001fd746a82cecc72b8981
sakshis-06 <115691471+sakshis-06@users.noreply.github.com>
2022-10-13 13:12:48 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,16 @@
+#how to create using taking input
+
+#using input keyword
+n=input("enter something..")
+print("the value entered is :",n)
+
+#to check type
+print(type(n))
+
+
+#typecaste the input using int
+m=input("enter something..")
+print("the value entered is :",m)
+
+#to check type
+print(type(m))
