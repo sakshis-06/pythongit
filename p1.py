a50c31dd12262892a546be50daaa3c7c461bf025
sakshis-06 <115691471+sakshis-06@users.noreply.github.com>
2022-10-13 13:08:51 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,14 @@
+#to print
+#1
+print('hello')
+name="abc"
+#2 using statement and variable
+print("hello" ,name)
+#3 using f format
+age=25
+print(f"the name is {name} and age is {age}   ")
+#4 using .format
+print("the name is {} and age is :{}   " .format(age,name))
+
+
+#to print in single line use end=
